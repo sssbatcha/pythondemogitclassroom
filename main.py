daebc27467d99e1b03a456346357051d4ac2e88a
green <green@example.com>
2025-08-26 12:57:55 +0530

    Added floor in main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import add,sub,mul,div
+import add,sub,mul,div,floor
 print("Addition is:",add.add(10,5))
 print("Subtraction is:",sub.sub(10,5))
 print("Multiplication is:",mul.mul(10,5))
 print("Division is:",div.div(10,5))
+print("Floor Division is:",floor.floor(10,5))
